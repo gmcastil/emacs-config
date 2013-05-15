aad9ee3abc6b7c56441569336c7eb35bce59c68d
George Castillo <gmcastil@gmail.com>
2013-05-15 00:24:22 -0600

    Initial project version

---
+++ init.el
@@ -0,0 +1,15 @@
+;; Configure load path
+;; This is for my own Lisp files
+(add-to-list 'load-path "~/.emacs.d/lisp/")
+;; This is for Lisp that isn't installed by package.el
+(add-to-list 'load-path "~/.emacs.d/site-lisp")
+
+;; Emacs 24 calls 'package-initialize to load all installed packages, but
+;; only after the entire init file is loaded.
+(load "ui-config.el")
+;; (load "package-manager.el")
+;; (load "erc-config.el")
+;; (load "slime-config.el")
+;; (load "python-config.el")
+;; (load "modeline-config.el")
+;; (load "pyflakes-config.el")
